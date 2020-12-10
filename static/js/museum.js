fc80382b465b2cc2b35deeb401d8b24654227700
jimmyliaviva <jimmy.liao86@gmail.com>
2020-12-10 15:30:08 +0800

    comment function finished V1.1

--- static/js/museum.js
+++ static/js/museum.js
@@ -11,15 +11,6 @@ $(document).ready(function () {
         },
         success: function (data) {
             appendComment(data)
-            // for (var cid in data) {
-            //     if (userId == data[cid].userId) {
-            //         $('#commentBlock').prepend('<div id="two_layer" class="row comment"> <div class="imgWrapper headWrapper col-1"><img src="../static/img/owl.png" alt=""></div> <div id="test" class="col-10"> <div class="userName">' + data[cid].user + ' <button id="remove" class="remove" type="button"value="' + cid + '">刪除 </button> <button id="edit">修改</button> <span class="time">' + data[cid].commentTime + '</span> </div> <div class="commentContent">' + data[cid].comment + '</div> </div> </div>')
-            //     } else {
-            //         $('#commentBlock').prepend('<div id="two_layer" class="row comment"><div class="imgWrapper headWrapper col-1"><img src="../static/img/owl.png" alt=""></div><div id="test" class="col-10"><div class="userName">' + data[cid].user + '</div><div class="commentContent">' + data[cid].comment + '</div></div></div>')
-            //
-            //     }
-            // }
-
         },
     })
     // 按下刪除留言
@@ -57,14 +48,6 @@ $(document).ready(function () {
                 // 方法二
                 $('div.commentBlock').children().remove()
                 appendComment(data)
-                // for (let cid in data) {
-                //     if (userId == data[cid].userId) {
-                //         $('#commentBlock').prepend('<div id="two_layer" class="row comment"><div class="imgWrapper headWrapper col-1"><img src="../static/img/owl.png" alt=""></div><div id="test" class="col-10"><div class="userName">' + data[cid].user + '</div><div class="commentContent">' + data[cid].comment + '</div></div><button id="remove" class="remove" type="button" value="' + cid + '">刪除</button></div>')
-                //     } else {
-                //         $('#commentBlock').prepend('<div id="two_layer" class="row comment"><div class="imgWrapper headWrapper col-1"><img src="../static/img/owl.png" alt=""></div><div id="test" class="col-10"><div class="userName">' + data[cid].user + '</div><div class="commentContent">' + data[cid].comment + '</div></div></div>')
-                //
-                //     }
-                // }
             }
         })
     });
